f686a89e7c3d1582821c524ce29060f426d7cad6
najongjine <najongjin3@hotmail.com>
2025-12-12 09:13:57 +0900

    .

--- app/(tabs)/Search.tsx
+++ app/(tabs)/Search.tsx
@@ -1,13 +1,28 @@
+import { useFocusEffect } from "@react-navigation/native";
 import { useLocalSearchParams } from "expo-router";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { useCallback } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Search() {
   const queryString = useLocalSearchParams();
   const category = queryString?.category ?? "";
+
+  useFocusEffect(
+    useCallback(() => {
+      //화면이 포커싱 되었을 때 실행시킬 내용을 작성
+      // return (
+      // 	//화면에서 포커싱이 떠났을 때 실행시킬 내용을 작성
+      // );
+    }, [])
+  );
+
   return (
     <ScrollView>
       <View>
         <Text>검색화면, 넘겨받은 데이터: {category}</Text>
+      </View>
+      <View>
+        <TextInput placeholder="검색어 입력" />
       </View>
     </ScrollView>
   );
