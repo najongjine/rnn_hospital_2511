81019c19e6176a14cab0595f03e0e2eb411ed6ca
najongjine <najongjin3@hotmail.com>
2025-12-12 09:20:23 +0900

    .

--- app/(tabs)/Search.tsx
+++ app/(tabs)/Search.tsx
@@ -4,16 +4,30 @@ import { useCallback } from "react";
 import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Search() {
+  const apiUrl = process.env.EXPO_PUBLIC_HONO_API_BASEURL;
   const queryString = useLocalSearchParams();
   const category = queryString?.category ?? "";
 
+  async function getHospital() {
+    const params = new URLSearchParams();
+    params.append("query", String(category)); // 사용자 검색어
+    params.append("x", x); // longitute
+    params.append("y", y); // langitute
+
+    const response = await fetch(`${apiUrl}?${params}`, {
+      method: "GET",
+      headers: {},
+    });
+
+    let _data = await response.json();
+  }
+
   useFocusEffect(
     useCallback(() => {
-      //화면이 포커싱 되었을 때 실행시킬 내용을 작성
       // return (
       // 	//화면에서 포커싱이 떠났을 때 실행시킬 내용을 작성
       // );
-    }, [])
+    }, [category])
   );
 
   return (
